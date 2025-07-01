7e72cb5914bbab42f9a5a45816953f5bc069f04b
mahesh joshi <maheshjoshi9373@gmail.com>
2025-07-01 15:22:18 +0530

    modified app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     greet(name)
     
     num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
+    num2 = float(input("Enter second number: joshi  "))
     
     result = add_numbers(num1, num2)
     print(f"The sum of {num1} and {num2} is {result}.")
